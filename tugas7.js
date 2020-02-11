b3e6b7c45676123ecee4fc8507c641d02bea3597
s-prianto <prianto2796@gmail.com>
2020-02-12 06:19:58 +0700

    Tugas 7 JS

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggi_badan= [180,179,178,177,176,175,174,173,172,171]
+
+for (let x of tinggi_badan){
+  console.log("tinggi badan siswa adalah",x);
+}
+
+console.log("");
+
+for(var i=0;i<tinggi_badan.length;i++){
+  console.log("tinggi bandan murid adalah",tinggi_badan[i]);
+}
